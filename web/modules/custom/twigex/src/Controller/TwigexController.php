e4d0b5c3847df353ccac45bebb89c4b14d697b9c
esokia <divesh.heeramun1@gmail.com>
2019-07-01 15:14:20 +0400

    Sharing of data from controller to template added

--- web/modules/custom/twigex/src/Controller/TwigexController.php
+++ web/modules/custom/twigex/src/Controller/TwigexController.php
@@ -9,15 +9,21 @@ use Drupal\Core\Controller\ControllerBase;
  */
 class TwigexController extends ControllerBase {
 
+  public $content = 'Hello from the darkside.';
+
   /**
    * Generate.
    *
    * @return string
-   *   Return Hello string.
+   * Return Hello string.
+   * 
+   * #content is being shared to the template.
+   * 
    */
   public function generate() {
     return [
       '#theme' => 'twigex',
+      '#content' => $this->t($this->content),
     ];
   }
 
