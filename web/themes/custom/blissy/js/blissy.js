0114e7447a95f5618de24d0c975a3c559b45f62e
esokia <divesh.heeramun1@gmail.com>
2019-07-01 11:28:15 +0400

    Added some basics JavaScript and CSS

---
+++ web/themes/custom/blissy/js/blissy.js
@@ -0,0 +1,21 @@
+console.log('Testing');
+
+/**
+ * Function making use of Drupal 8 JS.
+ * 
+ * This function simply checks if the DOM is ready
+ * and performs a logging.
+ */
+(function ($, Drupal) {
+    'use strict';
+    Drupal.behaviors.name = {
+      attach: function (context, settings) {
+        $(".selector", context)
+      }
+    }
+  
+    $(document).ready(function() {
+      console.log('DOM is ready!');
+    });
+  
+  })(window.jQuery, window.Drupal);
