1b395610da9ada6f1304f3072744ea64fc632e73
esokia <divesh.heeramun1@gmail.com>
2019-07-04 13:59:39 +0400

    Implemented boilerplate of the alterRoute method

--- web/modules/custom/helloworld/src/Routing/RouteSubscriber.php
+++ web/modules/custom/helloworld/src/Routing/RouteSubscriber.php
@@ -16,5 +16,18 @@ class RouteSubscriber extends RouteSubscriberBase {
    * {@inheritdoc}
    */
   protected function alterRoutes(RouteCollection $collection) {
+    /**
+     * In this case, this part of the code is not working.
+     * The `get` method takes the route machine name (use Devel)
+     * to find out.
+     * 
+     * The `setPath` method takes a destination route which
+     * needs to be set up prior to the migration.
+     */
+    if ($route = $collection->get('helloworld.content')) {
+      // $route->setPath('/contribute');
+    } else {
+      // Handle something if the route could not be found.
+    }
   }
 }
