0ecd9aa29c81550a8de1362c0a1d9dff1ecd2f26
esokia <divesh.heeramun1@gmail.com>
2019-07-08 11:15:42 +0400

    Dependency Injection service implemented for ingredients

--- web/modules/custom/ingredients/src/Controller/IngredientsController.php
+++ web/modules/custom/ingredients/src/Controller/IngredientsController.php
@@ -3,11 +3,29 @@
 namespace Drupal\ingredients\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use \Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Class IngredientsController.
  */
 class IngredientsController extends ControllerBase {
+
+  private $ingredientsGenerator;
+
+  /**
+   * Creating a container with the factory method.
+   */
+  public static function create(ContainerInterface $container) {
+    $ingredientsGenerator = $container->get('simpleservice.hello');
+    return new static($ingredientsGenerator);
+  }
+
+  /**
+   * Creating an instance of the service.
+   */
+  public function __construct($ingredientsGenerator) {
+    $this->ingredientsGenerator = $ingredientsGenerator;
+  }
 
   /**
    * Generate.
@@ -16,10 +34,11 @@ class IngredientsController extends ControllerBase {
    *   Return Hello string.
    */
   public function generate() {
-    $simpleservice = \Drupal::service('simpleservice.hello');
+    //$simpleservice = \Drupal::service('simpleservice.hello');
     return [
       '#theme' => 'ingredients',
-      '#messagevar' => $simpleservice->sayHello("Ingredients calling the service."),
+      //'#messagevar' => $simpleservice->sayHello("Ingredients calling the service."), // This one is for using the global $simpleservice
+      '#messagevar' => $this->ingredientsGenerator->sayHello("Hello from the dependency injection..."), // This one is using Dependency Injection and container
       // '#dumpvar' => $simpleservice->dumpSomething(),
     ];
   }
