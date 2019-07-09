0991f31b1880da87fd644b36af356df56f2e090d
esokia <divesh.heeramun1@gmail.com>
2019-07-09 11:00:31 +0400

    Successfully implemented a custom panel for a setting in the admin console

---
+++ web/modules/custom/menus/src/Controller/MenusController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\menus\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Class MenusController.
+ */
+class MenusController extends ControllerBase {
+
+  public function initMenu() {
+    return [
+      '#type' => 'markup',
+      '#markup' => $this->t('This is the menu settings page.')
+    ];
+  }
+
+  public function initThirdParty() {
+    return [
+      '#type' => 'markup',
+      '#markup' => $this->t('This is the third party settings page.')
+    ];
+  }
+
+}
