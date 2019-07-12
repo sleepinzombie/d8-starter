4a99345c64685788ddd3c25a1d7b90311e4cf33a
esokia <divesh.heeramun1@gmail.com>
2019-07-12 09:41:16 +0400

    Initialized module for trying out events and events subscribers

---
+++ web/modules/custom/events/src/CustomEventsInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Drupal\events;
+
+/**
+ * Interface CustomEventsInterface.
+ */
+interface CustomEventsInterface {
+
+
+}
---
+++ web/modules/custom/events/src/DefaultService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\events;
+
+/**
+ * Class DefaultService.
+ */
+class DefaultService implements CustomEventsInterface {
+
+  /**
+   * Constructs a new DefaultService object.
+   */
+  public function __construct() {
+
+  }
+
+}
