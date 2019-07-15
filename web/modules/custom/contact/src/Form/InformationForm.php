f9dd0fc6b3e9eda2d7617351efc67ddd93a58ca6
esokia <divesh.heeramun1@gmail.com>
2019-07-15 09:47:07 +0400

    Implemented the State API for saving title and content for Contact backoffice data

--- web/modules/custom/contact/src/Form/InformationForm.php
+++ web/modules/custom/contact/src/Form/InformationForm.php
@@ -22,17 +22,21 @@ class InformationForm extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+    $state_values = \Drupal::state()->getMultiple(['title', 'content']);
+
     $form['title'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Title'),
       '#maxlength' => 64,
       '#size' => 64,
       '#weight' => '0',
+      '#default_value' => $state_values['title'],
     ];
     $form['content'] = [
       '#type' => 'textarea',
       '#title' => $this->t('Content'),
       '#weight' => '0',
+      '#default_value' => $state_values['content'],
     ];
     $form['submit'] = [
       '#type' => 'submit',
@@ -58,12 +62,14 @@ class InformationForm extends FormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $title = $form_state->getValue('title');
     $content = $form_state->getValue('content');
-    \Drupal::messenger()->addMessage($title . $content);
 
-    // foreach ($form_state->getValues() as $key => $value) {
-    //   \Drupal::messenger()->addMessage($value);
-    //   // \Drupal::messenger()->addMessage($key . ': ' . ($key === 'text_format'?$value['value']:$value));
-    // }
+    $state_form_variables = [
+      'title' => $title,
+      'content' => $content,
+    ];
+
+    \Drupal::state()->setMultiple($state_form_variables);
+    \Drupal::messenger()->addMessage('Your content has been saved to the Drupal state.');
   }
 
 }
